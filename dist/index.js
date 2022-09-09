b0ee5a2d3b48e7f62f2a639475d4108ca6fa6d68
Stanislaw Nowak <stanislaw.nowak@cscharf.com>
2022-09-09 20:54:14 +0200

    Update

--- dist/index.js
+++ dist/index.js
@@ -42103,37 +42103,36 @@ const core = __nccwpck_require__(3722);
 const resources = __nccwpck_require__(8164);
 const identity = __nccwpck_require__(5671);
 
-try {
-  const subscriptionId = process.env.AZURE_SUBSCRIPTION_ID;
-  const credential = new identity.DefaultAzureCredential();
-  const resourcesClient = new resources.ResourceManagementClient(credential, subscriptionId);
+async function createResourceGroup() {
+  try {
+    const name = core.getInput("name", { required: true, trimWhitespace: true }).toUpperCase();
+    const location = core.getInput("location", { required: true, trimWhitespace: true }).toLowerCase();
 
-  const name = core.getInput("name", { required: true, trimWhitespace: true }).toUpperCase();
-  const location = core.getInput("location", { required: true, trimWhitespace: true }).toLowerCase();
+    const subscriptionId = process.env.AZURE_SUBSCRIPTION_ID;
+    const credential = new identity.DefaultAzureCredential();
+    const resourcesClient = new resources.ResourceManagementClient(credential, subscriptionId);
 
-  resourcesClient.resourceGroups.createOrUpdate(
-    name,
-    {
-      location: location
-    })
-    .then(
-      result => {
-        const id = result.id;
-        const location = result.location;
-        const provisioning_state = result.properties.provisioningState;
+    const exists = (await resourcesClient.resourceGroups.checkExistence(name)).body;
 
-        core.setSecret(id);
-
-        core.setOutput("id", id);
-        core.setOutput("location", location);
-        core.setOutput("provisioning_state", provisioning_state);
-
-        core.info(`The resource group ${name} was created successfully.`);
+    const resourceGroup = await resourcesClient.resourceGroups.createOrUpdate(name,
+      {
+        location: location
       });
+
+    core.setSecret(resourceGroup.id);
+
+    core.setOutput("id", resourceGroup.id);
+    core.setOutput("location", resourceGroup.location);
+    core.setOutput("provisioning_state", resourceGroup.properties.provisioningState);
+
+    core.info(`The resource group ${name} was ${exists ? 'updated' : 'created'} successfully.`);
+  }
+  catch (error) {
+    core.setFailed(error.message);
+  }
 }
-catch (error) {
-  core.setFailed(error.message);
-}
+
+createResourceGroup();
 })();
 
 module.exports = __webpack_exports__;
--- index.js
+++ index.js
@@ -2,34 +2,33 @@ const core = require('@actions/core');
 const resources = require("@azure/arm-resources");
 const identity = require("@azure/identity");
 
-try {
-  const subscriptionId = process.env.AZURE_SUBSCRIPTION_ID;
-  const credential = new identity.DefaultAzureCredential();
-  const resourcesClient = new resources.ResourceManagementClient(credential, subscriptionId);
+async function createResourceGroup() {
+  try {
+    const name = core.getInput("name", { required: true, trimWhitespace: true }).toUpperCase();
+    const location = core.getInput("location", { required: true, trimWhitespace: true }).toLowerCase();
 
-  const name = core.getInput("name", { required: true, trimWhitespace: true }).toUpperCase();
-  const location = core.getInput("location", { required: true, trimWhitespace: true }).toLowerCase();
+    const subscriptionId = process.env.AZURE_SUBSCRIPTION_ID;
+    const credential = new identity.DefaultAzureCredential();
+    const resourcesClient = new resources.ResourceManagementClient(credential, subscriptionId);
 
-  resourcesClient.resourceGroups.createOrUpdate(
-    name,
-    {
-      location: location
-    })
-    .then(
-      result => {
-        const id = result.id;
-        const location = result.location;
-        const provisioning_state = result.properties.provisioningState;
+    const exists = (await resourcesClient.resourceGroups.checkExistence(name)).body;
 
-        core.setSecret(id);
-
-        core.setOutput("id", id);
-        core.setOutput("location", location);
-        core.setOutput("provisioning_state", provisioning_state);
-
-        core.info(`The resource group ${name} was created successfully.`);
+    const resourceGroup = await resourcesClient.resourceGroups.createOrUpdate(name,
+      {
+        location: location
       });
+
+    core.setSecret(resourceGroup.id);
+
+    core.setOutput("id", resourceGroup.id);
+    core.setOutput("location", resourceGroup.location);
+    core.setOutput("provisioning_state", resourceGroup.properties.provisioningState);
+
+    core.info(`The resource group ${name} was ${exists ? 'updated' : 'created'} successfully.`);
+  }
+  catch (error) {
+    core.setFailed(error.message);
+  }
 }
-catch (error) {
-  core.setFailed(error.message);
-}
+
+createResourceGroup();
